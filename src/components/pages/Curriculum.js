d79989ffd8bf7d34f13e4b29f92935e1b277d0b6
Saravana Raja <saravanaraja25@gmail.com>
2021-02-24 23:50:32 +0530

    changes

--- src/components/pages/Curriculum.js
+++ src/components/pages/Curriculum.js
@@ -11,13 +11,13 @@ function Curriculum() {
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        <a target="_blank" href="http://coe.kongu.edu/R2014/MSCSyllabus2014/R2014-MSc-I-X%20Sem-Curriculum%20and%20Syllabi.pdf">
+                        <a target="_blank" rel="noreferrer" href="http://coe.kongu.edu/R2014/MSCSyllabus2014/R2014-MSc-I-X%20Sem-Curriculum%20and%20Syllabi.pdf">
                             <h3 className="mb-4">Curriculum - 2014</h3>
                         </a>
-                        <a target="_blank" href="http://coe.kongu.edu/R2014/R2018/R2018-MSC.pdf">
+                        <a target="_blank" rel="noreferrer" href="http://coe.kongu.edu/R2014/R2018/R2018-MSC.pdf">
                             <h3 className="mb-4">Curriculum - 2018</h3>
                         </a>
-                        <a target="_blank" href="http://coe.kongu.edu/R2014/PGSyllabus2020/R2020-MSC.pdf">
+                        <a target="_blank" rel="noreferrer" href="http://coe.kongu.edu/R2014/PGSyllabus2020/R2020-MSC.pdf">
                             <h3 className="mb-4">Curriculum - 2020</h3>
                         </a>
                     </div>
